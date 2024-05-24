29ceed0ecf192fc22905b337cf58649786600697
MOHAMMED KAIF <146006914+MKAIF5@users.noreply.github.com>
2024-05-24 16:41:39 +0500

    APIS PRACTICE WITH JON

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const url = "https://cat-fact.herokuapp.com";
 const getFacts  =  async () =>{
     let promise = await fetch(url)
     console.log(promise);
-    let data =  await promise.json();
+    let data =  await  promise.json();
     console.log(data);
 }
-
 getFacts();
